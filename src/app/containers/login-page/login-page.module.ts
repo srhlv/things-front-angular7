1eeee155eea448fef825835cd2e6148a0ee03f36
Sergey Levandovskyi <liovareg@gmail.com>
2018-11-20 10:38:20 +0200

    e

--- src/app/containers/login-page/login-page.module.ts
+++ src/app/containers/login-page/login-page.module.ts
@@ -2,11 +2,15 @@ import { NgModule } from "@angular/core";
 import { SharedModule } from "../shared/shared.module";
 import { LoginPageComponent } from "./login-page.component";
 import { LoginPageRoutingModule } from "./login-page-routing.module";
+import { StoreModule } from '@ngrx/store';
+import * as fromLoginPage from './login-page.reducer';
+import { EffectsModule } from '@ngrx/effects';
+import { LoginPageEffects } from './login-page.effects';
 
 @NgModule({
     declarations: [
         LoginPageComponent
     ],
-    imports: [SharedModule, LoginPageRoutingModule]
+    imports: [SharedModule, LoginPageRoutingModule, StoreModule.forFeature('loginPage', fromLoginPage.reducer), EffectsModule.forFeature([LoginPageEffects])]
 })
 export class LoginPageModule { }
---
+++ src/app/containers/login-page/login-page.reducer.spec.ts
@@ -0,0 +1,13 @@
+import { reducer, initialState } from './login-page.reducer';
+
+describe('LoginPage Reducer', () => {
+  describe('unknown action', () => {
+    it('should return the initial state', () => {
+      const action = {} as any;
+
+      const result = reducer(initialState, action);
+
+      expect(result).toBe(initialState);
+    });
+  });
+});
